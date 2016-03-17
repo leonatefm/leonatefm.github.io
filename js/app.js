80f953caebfa4f6d5d5df4c927ab4cda04cc878a
Chengqi Zhu <zhuc@us.ibm.com>
2016-03-16 22:09:12 -0500

    Mobile support for Project Detail Modal

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ $(document ).ready(function(){
 	$('.projects-list li').on('click', function(){
 		var index = $(this).index();
 		//show project modal
-		$('body').addClass('modal-open');
 		$('.projects-modal').show(0, function(){
 			$('.projects-modal').addClass('show');
 			$('.project-detail-list').show(0, function(){
 				$('.project-detail').eq(index).show(0, function(){
 					window.setTimeout(function(){
 						$('.project-detail').eq(index).find('.leftdiv, .rightdiv').addClass('show');
+						$('html, body').addClass('modal-open');
 					}, 500);
 				});
 			})
@@ -32,7 +32,7 @@ $(document ).ready(function(){
 		window.setTimeout(function(){
 			$('.project-detail, .project-detail-list, .projects-modal').hide();
 		}, 900);
-		$('body').removeClass('modal-open');
+		$('html, body').removeClass('modal-open');
 	};
 
 	//Click close icon to dismiss modal
@@ -46,4 +46,41 @@ $(document ).ready(function(){
 				dismissModal();
 		};
 	});
+
+	// //iOS Overlay Scroll Bug fixing
+	// document.ontouchmove = function ( event ) {
+
+ //        var isTouchMoveAllowed = true, target = event.target;
+
+ //        while ( target !== null ) {
+ //            if ( target.classList && target.classList.contains( 'modal-open' ) ) {
+ //                isTouchMoveAllowed = false;
+ //                break;
+ //            }
+ //            target = target.parentNode;
+ //        }
+
+ //        if ( !isTouchMoveAllowed ) {
+ //            event.preventDefault();
+ //        }
+
+ //    };
+
+ //    function removeIOSRubberEffect( element ) {
+
+ //        element.addEventListener( "touchstart", function () {
+
+ //            var top = element.scrollTop, totalScroll = element.scrollHeight, currentScroll = top + element.offsetHeight;
+
+ //            if ( top === 0 ) {
+ //                element.scrollTop = 1;
+ //            } else if ( currentScroll === totalScroll ) {
+ //                element.scrollTop = top - 1;
+ //            }
+
+ //        } );
+
+ //    }
+
+ //    removeIOSRubberEffect( document.querySelector( ".mobile-scrollable" ) );
 });
